a390ed10505767c7b08dda89053bf1855b90b62c
Tom - MBP <wiredogDesign@icloud.com>
2021-09-10 08:18:33 +0100

    Layout on desktop only for now

--- _assets/custom.js
+++ _assets/custom.js
@@ -1,7 +1,7 @@
 var trigger = document.querySelector('footer');
 var info = document.querySelector('.info');
 var imageCount = document.querySelectorAll('.thumbnail-trigger').length-1 + " dogs + 1 cat = wiredog design";
-document.querySelector('.gallery').setAttribute('data-image-counter', imageCount);
+document.querySelector('header').setAttribute('data-image-counter', imageCount);
 //
 function toggleClass() {
     info.classList.toggle('show-info');
